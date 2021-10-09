4c34abe649826769cf58ea37e1246d059cc2a390
kiteB <mmy789@sch.ac.kr>
2021-10-10 01:24:56 +0900

    Hello JPA - 애플리케이션 개발

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,49 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //등록
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//
+//            em.persist(member);
+//
+//            tx.commit();
+
+            //조회
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+//
+//            tx.commit();
+
+            //수정
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+            tx.commit();
+
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
