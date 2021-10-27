89aee82b2c63d1e0d05795bf53e7dc40066653c0
kiteB <mmy789@sch.ac.kr>
2021-10-28 02:02:25 +0900

    회원 수정 API

--- jpashop2/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop2/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -34,7 +34,7 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
-        if(!findMembers.isEmpty()) {
+        if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
@@ -48,5 +48,14 @@ public class MemberService {
     
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
+    }
+
+    /**
+     * 회원 수정
+     */
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
     }
 }
