f50834445ec45d46a0be30ccc682481b943914bb
kiteB <mmy789@sch.ac.kr>
2021-10-29 16:04:09 +0900

    사용자 정의 리포지토리 구현

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -296,4 +296,9 @@ class MemberRepositoryTest {
         //when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
