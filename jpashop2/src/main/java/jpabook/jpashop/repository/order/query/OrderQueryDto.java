11d6c95748fdbe94f13de33eb15caaa90835f167
kiteB <mmy789@sch.ac.kr>
2021-11-26 17:32:26 +0900

    주문 조회 V4: JPA에서 DTO 직접 조회

--- jpashop2/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ jpashop2/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -1,21 +1,25 @@
-package jpabook.jpashop.repository.order;
+package jpabook.jpashop.repository.order.query;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
-public class OrderSimpleQueryDto {
+@EqualsAndHashCode(of = "orderId")
+public class OrderQueryDto {
 
     private Long orderId;
     private String name;
-    private LocalDateTime orderDate;    //주문 시간
+    private LocalDateTime orderDate; //주문시간
     private OrderStatus orderStatus;
     private Address address;
+    private List<OrderItemQueryDto> orderItems;
 
-    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
         this.orderId = orderId;
         this.name = name;
         this.orderDate = orderDate;
--- jpashop2/src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
+++ jpashop2/src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop.repository.order;
+package jpabook.jpashop.repository.order.simplequery;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
